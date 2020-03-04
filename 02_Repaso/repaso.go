7fc2c504f89bb06aa339c28da217a1f0024eaa7a
Jhosep Davila <jhosepdb149@gmail.com>
2020-03-04 16:45:33 -0300

    Agregando carpetas

---
+++ 02_Repaso/repaso.go
@@ -0,0 +1,13 @@
+package main // Declaracion de paquete
+
+import "fmt" // Importar un paquete
+
+func main() { // funcion  Principal "main"
+	fmt.Println("Learn Go is very fun.") // Declacarion
+}
+
+/*
+	Comando de ayuda:
+	example
+	$ go doc fmt Println
+*/
